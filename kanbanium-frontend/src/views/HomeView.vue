73b22b88db37062aab3b7c596751ff14772876e8
icydevelop <chaekplayyt@gmail.com>
2023-08-15 12:20:58 +0300

    initial

---
+++ kanbanium-frontend/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    
+</script>
+<template>
+    <section class=" p-10">
+        <h1 class="text-5xl text-center mb-6">Test page</h1>
+        <div class=" grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-6">
+            <div class="card hover:bg-zinc-100 rounded-lg flex flex-col items-center">
+                <div class="card-header text-xl p-6">hello world</div>
+                <div class="card-body px-6 pb-6">test test test</div>
+                <button class="btn-primary w-1/2 mb-6">Test</button>
+            </div>
+            <div class="card hover:bg-zinc-100 rounded-lg flex flex-col items-center">
+                <div class="card-header text-xl p-6">hello world</div>
+                <div class="card-body px-6 pb-6">test test test</div>
+                <button class="btn-primary w-1/2 mb-6">Test</button>
+            </div>
+            <div class="card hover:bg-zinc-100 rounded-lg flex flex-col items-center">
+                <div class="card-header text-xl p-6">hello world</div>
+                <div class="card-body px-6 pb-6">test test test</div>
+                <button class="btn-primary w-1/2 mb-6">Test</button>
+            </div>
+            <div class="card hover:bg-zinc-100 rounded-lg flex flex-col items-center">
+                <div class="card-header text-xl p-6">hello world</div>
+                <div class="card-body px-6 pb-6">test test test</div>
+                <button class="btn-primary w-1/2 mb-6">Test</button>
+            </div>
+        </div>
+        <p class="text-base">Hello</p>
+    </section>
+</template>
